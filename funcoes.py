08ef9502910a58b6065036f70d4fc32ec21acd5a
Luiz Henrique <luizhenriquesousad1@gmail.com>
2026-08-17 21:03:08 -0300

    feat: implementa gerenciamento de produtos

--- funcoes.py
+++ funcoes.py
@@ -1,8 +1,24 @@
+import unicodedata
+
+def normalizar_texto(texto):
+
+    texto_normalizado = []
+
+    nfkd = unicodedata.normalize('NFKD', texto)
+
+
+    for letra in nfkd:
+        if not unicodedata.combining(letra):
+            texto_normalizado.append(letra)
+
+    texto_normalizado = "".join(texto_normalizado)
+
+    return texto_normalizado.lower()
 
 def menu_principal():
-
+    separador()
     print("MENU")
-    print('-' * 30)
+    separador()
     print("1 - Cadastrar produto")
     print("2 - Cadastrar cliente")
     print("3 - Listar produtos")
@@ -10,7 +26,7 @@ def menu_principal():
     print("5 - Editar produto")
     print("6 - Remover produto")
     print("7 - Sair")
-    print('-' * 30)
+    separador()
 
 def cadastro_produto(produtos):
 
@@ -19,8 +35,8 @@ def cadastro_produto(produtos):
     produto = {}
     novo_id = len(produtos) + 1
 
-    produto["id"] = novo_id
-    produto["nome"] = str(input("Insira o nome do produto: "))
+    produto["ID"] = novo_id
+    produto["nome"] = normalizar_texto(str(input("Insira o nome do produto: ")))
     produto["preço"] = float(input("Insira o preço do produto: "))
     produto["estoque"] = int(input("Insira a quantidade do produto em estoque: "))
 
@@ -42,19 +58,154 @@ def listar_produtos(produtos):
 
     for produto in produtos:
 
-        print("-"*30)
+        separador()
         print(f"ID - {produto["ID"]}")
         print(f"produto:....{produto["nome"]}")
         print(f"preço:......R${produto["preço"]:.2f}")
         print(f"estoque:....{produto["estoque"]}")
-        print("-"*30)
+        separador()
 
 
-def buscar_produto():
-    pass
+def buscar_produto(produtos):
+    print("buscar produto")
+    print("Buscar produto por ID.....[1]")
+    print("Buscar produto por nome...[2]")
 
-def editar_produto():
-    pass
+    opcao = int(input("Digite a opção desejada: "))
 
-def remove_produto():
-    pass
+    if opcao == 1:
+
+        buscador = int(input("Insira o ID do produto: "))
+        resultado = next((produto for produto in produtos if produto["ID"] == buscador), False)
+
+        if resultado == False:
+            print("produto não encontrado")
+            return False
+
+        else:
+            return resultado
+            """separador()
+            print(f"ID ... {resultado['ID']}")
+            print(f"nome ... {resultado['nome']}")
+            print(f"preço ...{resultado['preço']}")
+            print(f"estoque....{resultado['estoque']}")
+            separador()"""
+
+    elif opcao == 2:
+
+        buscador = []
+
+        nfkd = str(input("Digite o nome do produto: ")).lower()
+        decodificacao = unicodedata.normalize('NFKD', nfkd)
+
+        for letra in decodificacao:
+            if not unicodedata.combining(letra):
+
+                buscador.append(letra)
+
+        buscador = "".join(buscador)
+
+        resultado = next((produto for produto in produtos if produto["nome"] == buscador), False)
+
+        if resultado == False:
+            print("produto não encontrado")
+            return False
+
+        else:
+            return resultado
+            """"separador()
+            print(f"ID ... {resultado['ID']}")
+            print(f"nome ... {resultado['nome']}")
+            print(f"preço ...{resultado['preço']}")
+            print(f"estoque ....{resultado['estoque']}")
+            separador()"""
+
+
+def editar_produto(produtos):
+
+    produto = buscar_produto(produtos)
+
+    if produto == False:
+        return
+
+    """print("Produto encontrado: ")
+    print(produto)"""
+
+    separador()
+
+    print("nome.....[1]")
+    print("preço....[2]")
+    print("estoque....[3]")
+
+    opcao = int(input("Que opção deseja alterar?"))
+
+    if opcao == 1:
+
+        novo_nome = str(input("Insira o nome do produto: "))
+        produto["nome"] = normalizar_texto(novo_nome)
+        print("Nome do produto alterado com sucesso!")
+
+        print(f"nome do prodouto {produto["nome"]}")
+        print(f"preço do produto {produto["preço"]}")
+        print(f"estoque do produto {produto["estoque"]}")
+
+    elif opcao == 2:
+
+        novo_preco = float(input("Insira o novo preço do produto: "))
+        produto["preço"] = novo_preco
+        print("Preço do produto alterado com sucesso!")
+
+        print(f"nome do prodouto {produto["nome"]}")
+        print(f"preço do produto {produto["preço"]}")
+        print(f"estoque do produto {produto["estoque"]}")
+
+    elif opcao == 3:
+
+        novo_estoque = int(input("Insira o novo estoque do produto: "))
+        produto["estoque"] = novo_estoque
+        print("Estoque do produto alterado com sucesso!")
+
+        print(f"nome do prodouto {produto["nome"]}")
+        print(f"preço do produto {produto["preço"]}")
+        print(f"estoque do produto {produto["estoque"]}")
+
+def remover_produto(produtos):
+
+    produto = buscar_produto(produtos)
+
+    if produto == False:
+        return
+
+    print("deseja remover o produto ?")
+    print("Sim.....[1]")
+    print("Não.....[2]")
+
+    opcao = int(input("Digite a opção desejada: "))
+
+    if opcao == 1:
+        print("Tem certeza que deseja excluir o produto?")
+        print("Sim.....[1]")
+        print("Não.....[2]")
+
+        verificacao = int(input("Digite a opção desejada: "))
+
+        if verificacao == 1:
+
+            produtos.remove(produto)
+            print("Produto removido com sucesso!")
+
+
+        elif verificacao == 2:
+            return
+
+        """else:
+            print("Opção invalida!")
+            print("Digite uma opção valida:")
+            verificacao = int(input("Tem certeza que deseja excluir o produto?"))
+
+            print("Sim.....[1]")
+            print("Não.....[2]")"""
+
+
+def separador():
+    print("-"*30)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #importações
-from funcoes import menu_principal, cadastro_produto, cadastrar_cliente, listar_produtos
+import unicodedata
+from funcoes import (menu_principal, cadastro_produto, cadastrar_cliente, listar_produtos, buscar_produto,
+                     editar_produto, normalizar_texto, remover_produto)
 
 #Variáveis
-produtos = [{"ID": 1 ,"nome": "Café", "preço":8.5, "estoque": 50}, {"ID":2, "nome": "Capuccino", "preço": 10.0, "estoque": 50}]
+produtos = [{"ID": 1 ,"nome": "cafe", "preço":8.5, "estoque": 50}, {"ID":2, "nome": "capuccino", "preço": 10.0, "estoque": 50}]
 clientes = []
 
 #menu
@@ -25,7 +27,28 @@ while True:
     elif opcao == 3:
         listar_produtos(produtos)
 
+    elif opcao == 4:
+        print("Procurando...")
+        buscar_produto(produtos)
+
+    elif opcao == 5:
+
+        editar_produto(produtos)
+
+    elif opcao == 6:
+
+        remover_produto(produtos)
+
     elif opcao == 7:
         print("Salvando alterações")
         print("Finalizando...")
         break
+
+    elif opcao == 8:
+        print("Procurando...")
+        buscar_produto(produtos)
+
+    elif opcao == 9:
+        texto_teste = str(input("Digite alguma coisa ai: "))
+        normalizar_texto(texto_teste)
+        print(normalizar_texto(texto_teste))
